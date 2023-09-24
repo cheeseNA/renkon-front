e37d8f9fc1f9c65c8e53d69bc9b539b6de03032c
cheeseNA <kurocat2000@gmail.com>
2023-09-24 15:46:30 +0200

    Add api access funcs

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Room } from "@/types/types";
+import { Room, Person, Contact, ContentType } from "@/types/types";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 console.log("BASE_URL", BASE_URL);
@@ -9,5 +9,29 @@ if (!BASE_URL) {
 
 export async function getRoomByRefCode(ref_code: string): Promise<Room> {
   const response = await axios.get<Room>(`${BASE_URL}/room/${ref_code}`);
+  return response.data;
+}
+
+export async function createPerson(
+  room_id: string,
+  name: string
+): Promise<Person> {
+  const response = await axios.post<Person>(`${BASE_URL}/person/create`, {
+    name: name,
+    room_id: room_id,
+  });
+  return response.data;
+}
+
+export async function createContact(
+  person_id: string,
+  content_type: ContentType,
+  content: string
+): Promise<Contact> {
+  const response = await axios.post<Contact>(`${BASE_URL}/contact/create`, {
+    person_id: person_id,
+    content: content,
+    content_type: content_type,
+  });
   return response.data;
 }
